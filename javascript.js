6c3518c7d030c93fd969c4fb0b3f8297c187eb8a
H-hidetaka <sansanhidetaka@gmail.com>
2022-10-22 08:56:12 +0900

    [Add]javascript

---
+++ javascript.js
@@ -0,0 +1,42 @@
+// var message = 'こんち';
+// var gleet = prompt('おはおは');
+// alert(message + gleet);
+
+var weight = 'こんち';
+var weight = prompt('BMIを測定します。まずはあなたの体重を入力してください');
+var height;
+height = prompt('BMIを測定します。次にあなたの身長を測定します。');
+// 体重と身長からBMIを算出する
+var bmi = weight / (height * height);
+var message = 'あなたBMIは「' + bmi + '」です。';
+alert(message);
+
+var a = b / (c * c);
+
+// BMIの計算
+var a = b / (c * c);
+var bmi = weight /
+
+var budget = 2000;
+if (budget >= 1500) {
+  alert('ピザを注文します');
+}
+
+if (budget > 1500) {
+  alert('ピザを注文します');
+}
+if (budget >= 1500) {
+  alert('ピザを注文します');
+}
+if (budget < 1500) {
+  alert('ピザを注文します');
+}
+if (budget == 1500) {
+  alert('ピザを注文します');
+}
+if (budget !== 1500) {
+  alert('ピザを注文します');
+}
+if (budget === 1500) {
+  alert('ピザを注文します');
+}
